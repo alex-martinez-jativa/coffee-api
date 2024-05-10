5ecb8a6cef56911bea55222e81d6267f7339bae2
Alex Martinez Jativa <alex.martinez.jativa@gmail.com>
2024-05-10 16:57:22 +0800

    update routes

--- src/interface/routes.ts
+++ src/interface/routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { RecipeController } from '../application/recipeController';
-import { RecipeRepository } from 'src/infrastructure/recipesRepository';
+import { RecipeRepository } from '../infrastructure/recipesRepository';
 
 const router = Router();
 
 const recipeRepository = new RecipeRepository();
 const recipeController = new RecipeController(recipeRepository);
 
-router.get('/recipes', recipeController.getAll);
+router.get('/recipes', recipeController.getAll.bind(recipeController));
 
 export {
   router
